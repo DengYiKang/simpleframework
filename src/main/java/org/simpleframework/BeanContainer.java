a56b760076f8a7478a17a94305353d08f6e58fc0
yikang <1131487340@qq.com>
2021-04-22 20:01:32 +0800

    提供容器对外操作的方法

--- src/main/java/org/simpleframework/BeanContainer.java
+++ src/main/java/org/simpleframework/BeanContainer.java
@@ -11,10 +11,7 @@ import org.simpleframework.util.ClassUtil;
 import org.simpleframework.util.ValidationUtil;
 
 import java.lang.annotation.Annotation;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
@@ -101,4 +98,101 @@ public class BeanContainer {
         }
         loaded = true;
     }
+
+    /**
+     * 添加一个class对象及其Bean实例
+     *
+     * @param clazz class对象
+     * @param bean  bean实例
+     * @return 原有的bean实例，没有则返回null
+     */
+    public Object addBean(Class<?> clazz, Object bean) {
+        return beanMap.put(clazz, bean);
+    }
+
+    /**
+     * 移除一个IOC容器管理的对象
+     *
+     * @param clazz class对象
+     * @return 删除的bean实例，没有则返回null
+     */
+    public Object removeBean(Class<?> clazz) {
+        return beanMap.remove(clazz);
+    }
+
+    /**
+     * 根据class对象获取bean实例
+     *
+     * @param clazz class对象
+     * @return bean实例
+     */
+    public Object getBean(Class<?> clazz) {
+        return beanMap.get(clazz);
+    }
+
+    /**
+     * 获取容器管理的所有class对象集合
+     *
+     * @return class集合
+     */
+    public Set<Class<?>> getClasses() {
+        return beanMap.keySet();
+    }
+
+    /**
+     * 获取所有bean的集合
+     *
+     * @return bean集合
+     */
+    public Set<Object> getBeans() {
+        return new HashSet<>(beanMap.values());
+    }
+
+    /**
+     * 根据注解筛选出bean的class集合
+     *
+     * @param annotation 注解
+     * @return class集合
+     */
+    public Set<Class<?>> getClassesByAnnotation(Class<? extends Annotation> annotation) {
+        //1、获取beanMap的所有class对象
+        Set<Class<?>> keySet = getClasses();
+        if (ValidationUtil.isEmpty(keySet)) {
+            log.warn("nothing in beanMap");
+            return null;
+        }
+        Set<Class<?>> classSet = new HashSet<>();
+        //2、通过注解筛选出被注解标记的class对象，并添加到classSet中
+        for (Class<?> clazz : keySet) {
+            if (clazz.isAnnotationPresent(annotation)) {
+                classSet.add(clazz);
+            }
+        }
+        return classSet.size() > 0 ? classSet : null;
+    }
+
+    /**
+     * 通过接口或者父类获取实现类或者子类的class集合，不包括其本身
+     *
+     * @param interfaceOrClass 接口class或者父类class
+     * @return class集合
+     */
+    public Set<Class<?>> getClassesBySuper(Class<?> interfaceOrClass) {
+        //1、获取beanMap的所有class对象
+        Set<Class<?>> keySet = getClasses();
+        if (ValidationUtil.isEmpty(keySet)) {
+            log.warn("nothing in beanMap");
+            return null;
+        }
+        Set<Class<?>> classSet = new HashSet<>();
+        //2、判断keySet里的元素是否是传入的接口或者类的子类，如果是，就将其添加到classSet中
+        for (Class<?> clazz : keySet) {
+            //注意，isAssignableFrom还包括自己本身，需要排除在外
+            if (interfaceOrClass.isAssignableFrom(clazz) && !clazz.equals(interfaceOrClass)) {
+                classSet.add(clazz);
+            }
+        }
+        return classSet.size() > 0 ? classSet : null;
+    }
+
 }
--- src/test/java/org/simpleframework/ciore/BeanContainerTest.java
+++ src/test/java/org/simpleframework/ciore/BeanContainerTest.java
@@ -1,11 +1,14 @@
 package org.simpleframework.ciore;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.yikang.controller.DispatcherServlet;
+import com.yikang.controller.frontend.MainPageController;
+import com.yikang.service.solo.HeadLineService;
+import com.yikang.service.solo.impl.HeadLineServiceImpl;
+import org.junit.jupiter.api.*;
 import org.simpleframework.BeanContainer;
+import org.simpleframework.core.annotation.Controller;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BeanContainerTest {
     private static BeanContainer beanContainer;
 
@@ -15,11 +18,38 @@ public class BeanContainerTest {
     }
 
     @DisplayName("加载目标类及其实例到BeanContainer：loadBeansTest")
+    @Order(1)
     @Test
     public void loadBeansTest() {
         Assertions.assertEquals(false, beanContainer.isLoaded());
         beanContainer.loadBeans("com.yikang");
         Assertions.assertEquals(7, beanContainer.size());
         Assertions.assertEquals(true, beanContainer.isLoaded());
+    }
+
+    @DisplayName("根据获取其实例：getBeanTest")
+    @Order(2)
+    @Test
+    public void getBean() {
+        MainPageController mainPageController = (MainPageController) beanContainer.getBean(MainPageController.class);
+        Assertions.assertEquals(true, mainPageController instanceof MainPageController);
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) beanContainer.getBean(DispatcherServlet.class);
+        Assertions.assertEquals(null, dispatcherServlet);
+    }
+
+    @DisplayName("根据注解获取对应的实例：getClassesByAnnotationTest")
+    @Order(3)
+    @Test
+    public void getClassesByAnnotationTest() {
+        Assertions.assertEquals(true, beanContainer.isLoaded());
+        Assertions.assertEquals(3, beanContainer.getClassesByAnnotation(Controller.class).size());
+    }
+
+    @DisplayName("根据接口获取实现类：getClassesBySuperTest")
+    @Order(4)
+    @Test
+    public void getClassesBySuperTest() {
+        Assertions.assertEquals(true, beanContainer.isLoaded());
+        Assertions.assertEquals(true, beanContainer.getClassesBySuper(HeadLineService.class).contains(HeadLineServiceImpl.class));
     }
 }
